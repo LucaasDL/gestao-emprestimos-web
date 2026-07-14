47c85c9953821a3d967e0d7eeae982ec30bf8f70
Lucas <lucascombatarms500@gmail.com>
2026-07-14 11:23:05 -0300

    Cobrancas: aba Presencial (4+ cobrancas) (20260714112224)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1580220702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3495794591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
