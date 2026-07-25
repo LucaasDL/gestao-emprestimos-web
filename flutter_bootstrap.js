ac2424538a66e1234a7445d2f197106fb06bb369
Lucas <lucascombatarms500@gmail.com>
2026-07-25 10:24:49 -0300

    Score: analise de risco de emprestimo (20260725102352)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1447858931" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1521756803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
