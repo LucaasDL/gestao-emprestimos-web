dd992f02f739fd77a9f3dad6995e3e5efe67c5cb
Lucas <lucascombatarms500@gmail.com>
2026-09-11 11:17:39 -0300

    Menu do WhatsApp nao cresce depois de abrir (20260911111648)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "922333359" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1602057493" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
